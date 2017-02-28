db52f1cc23747361d3b0a31d3b79f48b5e7ec9df
niclego <nlegorre@lion.lmu.edu>
2017-02-27 20:09:01 -0800

    Test running

--- test/test.js
+++ test/test.js
@@ -1,7 +1,7 @@
 const ohm = require('../node_modules/ohm-js');
 const fs = require('fs');
 
-const contents = fs.readFileSync('../skrt.ohm');
+const contents = fs.readFileSync('skrt.ohm');
 const myGrammar = ohm.grammar(contents);
 
 const userInput = 'def x = 5;';
