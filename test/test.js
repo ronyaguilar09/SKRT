41f17a3f50dba20b7ed6bac395091cd97338e2c6
niclego <nlegorre@lion.lmu.edu>
2017-02-27 19:51:54 -0800

    Test ohm

---
+++ test/test.js
@@ -0,0 +1,13 @@
+const ohm = require('ohm-js');
+const fs = require('fs');
+
+const contents = fs.readFileSync('../skrt.ohm');
+const myGrammar = ohm.grammar(contents);
+
+const userInput = 'def x = 5;';
+const m = myGrammar.match(userInput);
+if (m.succeeded()) {
+  console.log('Greetings, human.');
+} else {
+  console.log("That's not a greeting!");
+}
