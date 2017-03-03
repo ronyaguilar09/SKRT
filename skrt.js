2d1db13622d090fb0e9b5d2587ad0c4030e59240
Rony Aguilar <raguil12@lion.lmu.edu>
2017-03-02 20:38:11 -0800

    Almost done with semantics

--- skrt.js
+++ skrt.js
@@ -269,11 +269,17 @@ class Boolean {
   constructor(value) {
     this.value = value;
   }
+  toString() {
+    return (`Bool: ${this.value}`);
+  }
 }
 
 class Integer {
   constructor(value) {
     this.value = value;
+  }
+  toString() {
+    return (`Int: ${this.value}`);
   }
 }
 
@@ -281,16 +287,26 @@ class String {
   constructor(value) {
     this.value = value;
   }
-}
+  toString() {
+    return (`String: ${this.value}`);
+  }
+ }
 class Float {
-  constructor(value) {
-    this.value = value;
+  constructor(value, value2) {
+    this.value1 = value;
+    this.value2 = value2;
+  }
+  toString() {
+    return (`Float: ${this.value}`);
   }
 }
 
 class Id {
-  constructor(value) {
-    this.value = value;
+  constructor(value, idrest) {
+    this.value = value + idrest;
+  }
+  toString() {
+    return (`Bool: ${this.value}`);
   }
 }
 
@@ -298,11 +314,17 @@ class IdRest {
   constructor(value) {
     this.value = value;
   }
+  toString() {
+    return (`Bool: ${this.value}`);
+  }
 }
 
 class CharLit {
   constructor(value) {
     this.value = value;
+  }
+  toString() {
+    return (`Bool: ${this.value}`);
   }
 }
 
@@ -310,42 +332,54 @@ class Char {
   constructor(value) {
     this.value = value;
   }
+  toString() {
+    return (`Bool: ${this.value}`);
+  }
 }
 
 
 const semantics = skrtGrammar.createSemantics().addOperation('tree', {
   Program(body) { return new Program(body.tree()); },
   Body(stmt) { return new Body(stmt.tree()); },
+  Def(def) { return new Definition(def.tree()); },
   Stmts(stmt) { return new Statement(stmt.tree()); },
   VarDef(_, id, _a, exp, _b) { return new VariableDefinition(id.sourceString, exp.tree()); },
   StructDef(_, id, _a, struct, _b) { return new StructDefinition(id.sourceString, struct.tree()); },
   FunDef(_, funName, params, _a, _b, body, _c) { return new FunctionDefinition(funName.sourceString, params.tree(), body.tree()); },
   CamlDef(_, type, _a, id, _b, exp, _c) { return new AssertDefinition(type.tree(), id.sourceString, exp.tree()); },
   ObjDef(_, id, _a, obj) { return new ObjectDefinition(id.sourceString, obj.tree()); },
-  BinExp(left, op, right) { return new BinaryExpression(left.tree(), op.sourceString, right.tree()); },
-  UnaryExp(op, operand) { return new UnaryExpression(op.sourceString, operand.tree()); },
-  ParensExp(p1, exp, p2) { return new ParensExpression(exp.tree()); },
-  NumLit(value) { return new NumericLiteral(value.sourceString); },
+  Exp_binary(left, op, right) { return new BinaryExpression(left.tree(), op.sourceString, right.tree()); },
+ // Exp_binary(left, op, right) { return new BinaryExpression(left.tree(), op.sourceString, right.tree()); },
+  Exp2_binary(left, op, right) { return new BinaryExpression(left.tree(), op.sourceString, right.tree()); },
+  Exp3_binary(left, op, right) { return new BinaryExpression(left.tree(), op.sourceString, right.tree()); },
+  Exp4_binary(left, op, right) { return new BinaryExpression(left.tree(), op.sourceString, right.tree()); },
+  Exp5_binary(left, op, right) { return new BinaryExpression(left.tree(), op.sourceString, right.tree()); },
+  Exp6_binary(left, op, right) { return new BinaryExpression(left.tree(), op.sourceString, right.tree()); },
+  Exp7_binary(left, op, right) { return new BinaryExpression(left.tree(), op.sourceString, right.tree()); },
+  Exp8(type) { return new BinaryExpression(type.tree()); },
+  // Exp_unary(op, operand) { return new UnaryExpression(op.sourceString, operand.tree()); },
+  Exp9_parens(p1, exp, p2) { return new ParensExpression(exp.tree()); },
+ // NumLit(value) { return new NumericLiteral(value.sourceString); },
   For(_, id, from, exp1, to, exp2, b1, body, b2) { return new For(id.sourceString, exp1.tree(), exp2.tree(), body.tree()); },
-  IfElse(_, _a, cond1, _b, body1, _c, _d, _e, cond2, _f, _g, body2, _h, _i, _j, body3, _k) { return new IfElse(cond1.tree(), body1.tree(), cond2.tree(), body2.tree(), body3.tree()); },
- // Else_(_, _a, exp1, _b, _c, body1, _d, _e, _f, body2, _g) { return new Else(exp1.tree(), body1.tree(), body2.tree()); },
-  MatchStmt(_, _a, exp, _b, _c, block) { return new Match(exp.tree(), block.tree()); },
-  MatchBlck(_, pattern, _a, stmt) { return new MatchBlock(pattern.tree(), stmt.tree()); },
+  IfElse(_, _a, cond1, p, _b, body1, _c, _d, _e, cond2, _f, _g, body2, _h, _i, _j, body3, _k) { return new IfElse(cond1.tree(), body1.tree(), cond2.tree(), body2.tree(), body3.tree()); },
+  // IfElse_else(_, _a, exp1, _b, _c, body1, _d, _e, _f, body2, _g) { return new Else(exp1.tree(), body1.tree(), body2.tree()); },
+  Match(_, _a, exp, _b, _c, block) { return new Match(exp.tree(), block.tree()); },
+  MatchBlock(_, pattern, _a, stmt) { return new MatchBlock(pattern.tree(), stmt.tree()); },
   MatchPat(exp) { return new MatchPattern(exp.tree()); },
-  StrucId(id) { return new StructId(id.sourceString); },
-  Type_(type) { return new Type(type.tree()); },
+  StructId(id) { return new StructId(id.sourceString); },
+  Type(type) { return new Type(type.tree()); },
   Prim(prim) { return new Primitive(prim.tree()); },
   Obj(openB, id, colon, exp, comma, lastId, lastColon, lastExp, lastCloseB) { return new Object(id.sourceString, exp.tree(), lastId.sourceString, lastExp.tree()); },
-  Tuple_(openP, exp, comma, lastExp, closeP) { return new Tuple(exp.tree(), lastExp.tree()); },
-  List_(openP, exp, comma, lastExp, closeP) { return new List(exp.tree(), lastExp.tree()); },
-  Bool(val) { return new Boolean(val.sourceString); },
-  Int(val) { return new Integer(val.sourceString); },
-  Str(val) { return new String(val.sourceString); },
-  Flt(val) { return new Float(val.sourceString); },
-  Id_(val) { return new Id(val.sourceString); },
-  Id_Rest(val) { return new IdRest(val.sourceString); },
-  Char_Lit(val) { return new CharLit(val.sourceString); },
-  Char_(val) { return new Char(val.sourceString); },
+  Tuple(openP, exp, comma, lastExp, closeP) { return new Tuple(exp.tree(), lastExp.tree()); },
+  List(openP, exp, comma, lastExp, closeP) { return new List(exp.tree(), lastExp.tree()); },
+  bool(val) { return new Boolean(val.sourceString); },
+  int(val) { return new Integer(val.sourceString); },
+  stringlit(p, val, p2) { return new String(val.sourceString); },
+  float(val, dot, val2) { return new Float(val.sourceString, val2.sourceString); },
+  id(val, idrest) { return new Id(val.sourceString, idrest.sourceString); },
+  idrest(val) { return new IdRest(val.sourceString); },
+  charlit(p1, val, p2) { return new CharLit(val.sourceString); },
+  char(val) { return new Char(val.sourceString); },
 });
 
 const match = skrtGrammar.match('def x = 5;');
