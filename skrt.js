404e6807cb89e0cafd3359d7a0d7ea6e34543db6
Rony Aguilar <raguil12@lion.lmu.edu>
2017-03-02 19:41:02 -0800

    File synced grammar

--- skrt.js
+++ skrt.js
@@ -1,58 +1,8 @@
 const ohm = require('ohm-js');
 
-const skrtGrammar = ohm.grammar(`SKRT {
-    Program     =  Body
-    Body        =  Stmts*
-    Stmts       =  Def
-              |  IfElse
-              |  For
-              |  Exp
-    Def         = VarDef | FunDef | CamlDef | ObjDef | StructDef
-    StructDef   = "def" StructId "=" StructId ";"
-    VarDef      = "def" id "=" Exp ";"
-    FunDef      = "def" id "=>" "{" Body "}"
-    CamlDef     = "def" Type ":" id "=" Exp";" // dont restrict just to primitive types here
-    ObjDef      = "type" id "=" Obj
-    IfElse      = "if" "(" Exp ")" "{" Body "}" ( "else if" "(" Exp ")" "{" Body "}" )* ("else" "{" Body "}")? --ifelse
-                |"if" "(" Exp ")" "{" Body "}" ("else" "{" Body "}")? --else
-    For         = "for" id "from" Exp "to" Exp "{" Body "}"
-    Match       = "match" "(" Exp ")" "with" MatchBlock
-    MatchBlock  = ("|" MatchPat "=>" Stmts)+
-    MatchPat    = Exp|"_"
-    StructId    = id | Tuple
-    Exp         = Exp1("or" Exp)*
-    Exp1        = Exp2("and" Exp1)*
-    Exp2        = Exp3(relop Exp2)*
-    Exp3        = Exp4(addop Exp3)*
-    Exp4        = Exp5(mulop Exp4)*
-    Exp5        = Exp6(preop Exp6)?
-    Exp6        = Exp7("**" Exp7)*
-    Exp7        = Exp8("." Exp8)?
-    Exp8        = Type | Match | Exp9
-    Exp9        = "(" Exp ")"
-    Type        = Prim | Tuple | List
-    Prim        = bool|int|stringlit|float|id|charlit
-    Obj         = "{" (~(id ":" Exp "}" ) (id ":" Exp "," ))* (id ":" Exp "}" )
-    Tuple       = "(" (~(Exp ")")(Exp","))* (Exp ")")
-    List        = "[" (~(Exp ")")(Exp ","))* (Exp "]")
-
-    bool        = "true" | "false"
-    int         = digit+
-    stringlit   = "\"" (char|'\'')* "\""
-    float       = digit* '.'' digit+
-    id          = ~keyword letter idrest*
-    idrest      = "_" | alnum
-    charlit     = '\'' any '\''
-    char        = escape
-                |~"\\" ~"\"" ~"'" ~"\n" any
-    relop       = "<"|">"|"<="|"=="|">="|"!="
-    addop       = "+" | "-"
-    mulop       = "*" | "/" | "%"
-    preop       = "-" | "!"
-    keyword     = ("for" | "match" | "def" | "type" | "from" | "to" | "with" | "if" | "else" | "or" | "and" | "true" | "false" | "print") ~idrest
-    escape      = "\\\\" | "\\\"" | "\\'" | "\\n" | "\\t"
-                | "\\u{"hexDigit+"}"                        -- codepoint
-}`);
+const fs = require('fs');
+const contents = fs.readFileSync('skrt.ohm');
+const skrtGrammar = ohm.grammar(contents);
 
 class Program {
   constuctor(body) {
@@ -108,13 +58,14 @@ class StructDefinition extends Definition {
 }
 
 class FunctionDefinition extends Definition {
-  constructor(_, id, _a, _b, body, _c) {
+  constructor(_, funName, params, _a, _b, body, _c) {
     super();
-    this.id = id;
+    this.id = funName;
+    this.params = params;
     this.body = body;
   }
   toString() {
-    return (`( Func: ${this.id} = ${this.body} )`);
+    return (`( Func: ${this.id} (${this.params})= ${this.body} )`);
   }
 }
 
@@ -356,38 +307,40 @@ const semantics = skrtGrammar.createSemantics().addOperation('tree', {
   Program(body) { return new Program(body.tree()); },
   Body(stmt) { return new Body(stmt.tree()); },
   Stmts(stmt) { return new Statement(stmt.tree()); },
-  VarDef(_, id, _a, exp, _b) { return new VariableDefinition(_, id.tree(), _, exp.tree()); },
-  StructDef(_, id, _a, struct, _b) {
-    return new StructDefinition(_, id.tree(), _a, struct.tree(), _b);
-  },
-  FuncDef(_, id, _a, _b, body, _c) { return new FunctionDefinition(_, id.tree(), _a, _b, body.tree(), _c); },
-  AssDef(_, type, _a, id, _b, exp, _c) { return new AssertDefinition(_, type.tree(), _a, id.tree(), _b, exp.tree(), _c); },
-  ObjDef(_, id, _a, obj) { return new ObjectDefinition(_, id.tree(), _a, obj.tree()); },
-  BinExp(left, op, right) { return new BinaryExpression(left.tree(), op.sourceString(), right.tree()); },
+  VarDef(_, id, _a, exp, _b) { return new VariableDefinition(id.sourceString, exp.tree()); },
+  StructDef(_, id, _a, struct, _b) { return new StructDefinition(id.sourceString, struct.tree()); },
+  FuncDef(_, funName, params, _a, _b, body, _c) { return new FunctionDefinition(funName.sourceString, params.tree(), body.tree()); },
+  AssDef(_, type, _a, id, _b, exp, _c) { return new AssertDefinition(type.tree(), id.sourceString, exp.tree()); },
+  ObjDef(_, id, _a, obj) { return new ObjectDefinition(id.sourceString, obj.tree()); },
+  BinExp(left, op, right) { return new BinaryExpression(left.tree(), op.sourceString, right.tree()); },
   UnaryExp(op, operand) { return new UnaryExpression(op.sourceString, operand.tree()); },
-  ParensExp(p1, exp, p2) { return new ParensExpression(p1, exp.tree(), p2); },
-  NumLit(value) { return new NumericLiteral(value.tree()); },
-  IfEl(_, _a, cond1, _b, body1, _c, _d, _e, cond2, _f, _g, body2, _h, _i, _j, body3, _k) { return new IfElse(_, _a, cond1.tree(), _b, body1.tree(), _c, _d, _e, cond2.tree(), _f, _g, body2.tree(), _h, _i, _j, body3.tree(), _k); },
-  Els(_, _a, exp1, _b, _c, body1, _d, _e, _f, body2, _g) { return new Else(_, _a, exp1.tree(), _b, _c, body1.tree(), _d, _e, _f, body2.tree(), _g); },
-  MatchStmt(_, _a, exp, _b, _c, block) { return new Match(_, _a, exp.tree(), _b, _c, block.tree()); },
-  MatchBlck(_, pattern, _a, stmt) { return new MatchBlock(_, pattern.tree(), _a, stmt.tree()); },
+  ParensExp(p1, exp, p2) { return new ParensExpression(exp.tree()); },
+  NumLit(value) { return new NumericLiteral(value.sourceString; },
+  IfEl(_, _a, cond1, _b, body1, _c, _d, _e, cond2, _f, _g, body2, _h, _i, _j, body3, _k) { return new IfElse(cond1.tree(), body1.tree(), cond2.tree(), body2.tree(), body3.tree()); },
+  Els(_, _a, exp1, _b, _c, body1, _d, _e, _f, body2, _g) { return new Else(exp1.tree(), body1.tree(), body2.tree()); },
+  MatchStmt(_, _a, exp, _b, _c, block) { return new Match(exp.tree(), block.tree()); },
+  MatchBlck(_, pattern, _a, stmt) { return new MatchBlock(pattern.tree(), stmt.tree()); },
   MatchPat(exp) { return new MatchPattern(exp.tree()); },
-  StrucId(id) { return new StructId(id.tree()); },
+  StrucId(id) { return new StructId(id.sourceString; },
   Typee(type) { return new Type(type.tree()); },
   Prim(prim) { return new Primitive(prim.tree()); },
-  Obj(openB, id, colon, exp, comma, lastId, lastColon, lastExp, lastCloseB) { return new Object(openB, id.tree(), colon, exp.tree(), comma, lastId.tree(), lastColon, lastExp.tree(), lastCloseB); },
-  Tup(openP, exp, comma, lastExp, closeP) { return new Tuple(openP, exp.tree(), comma, lastExp.tree(), closeP); },
-  List_(openP, exp, comma, lastExp, closeP) { return new List(openP, exp.tree(), comma, lastExp.tree(), closeP); },
-  Bool(val) { return new Boolean(val.sourceString()); },
-  Int(val) { return new Integer(val.sourceString()); },
-  Str(val) { return new String(val.sourceString()); },
-  Flt(val) { return new Float(val.sourceString()); },
-  Id_(val) { return new Id(val.sourceString()); },
-  Id_Rest(val) { return new IdRest(val.sourceString()); },
-  Char_Lit(val) { return new CharLit(val.sourceString()); },
-  Char_(val) { return new Char(val.sourceString()); },
+  Obj(openB, id, colon, exp, comma, lastId, lastColon, lastExp, lastCloseB) { return new Object(id.sourceString, exp.tree(), lastId.sourceString, lastExp.tree()); },
+  Tuple_(openP, exp, comma, lastExp, closeP) { return new Tuple(exp.tree(), lastExp.tree()); },
+  List_(openP, exp, comma, lastExp, closeP) { return new List(exp.tree(), lastExp.tree()); },
+  Bool(val) { return new Boolean(val.sourceString; },
+  Int(val) { return new Integer(val.sourceString; },
+  Str(val) { return new String(val.sourceString; },
+  Flt(val) { return new Float(val.sourceString; },
+  Id_(val) { return new Id(val.sourceString; },
+  Id_Rest(val) { return new IdRest(val.sourceString; },
+  Char_Lit(val) { return new CharLit(val.sourceString; },
+  Char_(val) { return new Char(val.sourceString; },
 });
 
+const match = skrtGrammar.match(`def x = 5;
+                               def y = x;
+                               x + y`);
+if (match.succeeded()) { console.log(semantics(match).tree()); }
 // read args
 // Parse
 // constole.log(.ast())
