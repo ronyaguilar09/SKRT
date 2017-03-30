f98f9270f64bcc37c4ee541941c9f96a2ee795fc
Kevin  Hamilton <khamilt7@lion.lmu.edu>
2017-03-29 17:39:57 -0700

    added analysis for boolLit

--- entities/boolean.js
+++ entities/boolean.js
@@ -1,8 +1,16 @@
+const Type = require('./type');
+
 class Boolean {
+
   constructor(value) {
     this.value = value;
   }
+
   toString() {
     return (`Bool: ${this.value}`);
+  }
+
+  analyze(context) {
+    this.type = Type.BOOL
   }
 }
