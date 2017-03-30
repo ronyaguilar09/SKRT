914de4b7f1b4615a4692d91115d6b8635e71a7ae
Kevin  Hamilton <khamilt7@lion.lmu.edu>
2017-03-29 17:57:23 -0700

    added analyze to float

--- entities/float.js
+++ entities/float.js
@@ -1,9 +1,17 @@
+const Type = require('./type');
+
 class Float {
+
   constructor(value, value2) {
     this.value1 = value;
     this.value2 = value2;
   }
+
   toString() {
     return (`Float: ${this.value1.join('')}. ${this.value2.join('')}`);
+  }
+  
+  analyze(context) {
+    this.type = type.FLOAT;
   }
 }
