102f397f1722845c4a423821ffd9a8a665089e96
Kevin  Hamilton <khamilt7@lion.lmu.edu>
2017-03-29 17:45:30 -0700

    added analysis for char

--- entities/char.js
+++ entities/char.js
@@ -1,8 +1,16 @@
+const Type = require('.type');
+
 class Char {
+
   constructor(value) {
     this.value = value;
   }
+
   toString() {
     return (`${this.value}`);
+  }
+
+  analyze(context) {
+    this.type = Type.CHAR;
   }
 }
