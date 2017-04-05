9eeee9b88869fe1818abffd29738c9a5ff65c742
niclego <nlegorre@lion.lmu.edu>
2017-04-05 15:33:26 -0700

    test are ducked

--- test/analyzer-test.js
+++ test/analyzer-test.js
@@ -1,10 +1,15 @@
-const fs = require('fs');
+const ohm = require('../node_modules/ohm-js');
 const assert = require('assert');
+
+const fs = require('fs');
+
+const contents = fs.readFileSync('../skrt.ohm');
+const myGrammar = ohm.grammar(contents);
 const parse = require('../skrt.js');
 
 describe('The semantic analyzer', () => {
   fs.readdirSync(__dirname).forEach((name) => {
-    if (name.endsWith('.skrt')) {
+    if (name.endsWith('.error')) {
       it(`detects a ${name.replace(/[^a-z]/g, ' ')}`, (done) => {
         const program = parse(fs.readFileSync(`${__dirname}/${name}`, 'utf-8'));
         const errorPattern = RegExp(name.replace('.error', '').replace(/-/g, ' '), 'i');
@@ -12,7 +17,7 @@ describe('The semantic analyzer', () => {
         assert.throws(() => program.analyze(), errorPattern);
         done();
       });
-    } else if (name.endsWith('.error')) {
+    } else if (name.endsWith('.skrt')) {
       it(`should analyze ${name} without errors`, (done) => {
         // For now, we are happy to know that these files pass semantic analysis.
         // We eventually need to check that the ASTs are properly decorated.
