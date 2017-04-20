069bc8f1ed75515f1981af6dc4dde75bd1c56cf5
Rony Aguilar <raguil12@lion.lmu.edu>
2017-04-20 13:44:48 -0700

    Added loop analyzer

--- entities/for.js
+++ entities/for.js
@@ -1,3 +1,6 @@
+const Type = require('./type');
+const Context = require('../analyzer');
+
 module.exports = class For {
   constructor(id, exp1, exp2, body) {
     this.id = id;
@@ -7,5 +10,17 @@ module.exports = class For {
   }
   toString() {
     return `( For: ${this.id} from ${this.exp1} to ${this.exp2} ${this.body})`;
+  }
+
+  analyze(context) {
+    this.localContext = context.createChildContextForLoop();
+    this.id.analyze(this.localContext);
+    this.exp1.analyze(this.localContext);
+    this.exp2.analyze(this.localContext);
+    if (!Type.isNumber(this.exp1.type.literal) || !Type.isNumber(this.exp2.type.literal)) {
+      throw Error('Expected numeric values in range for loop');
+    }
+    this.localContext.addVariable(this.id, this.exp1);
+    this.body.analyze(this.localContext);
   }
 };
