232546bfbac4b16bd95678840cd12f0b70e544c3
Filip Mietka <fmietka@lion.lmu.edu>
2017-04-26 22:41:29 -0700

    tests created but not working yet

--- test/generator-test.js
+++ test/generator-test.js
@@ -1,19 +1,46 @@
-const ohm = require('../node_modules/ohm-js');
+// const ohm = require('../node_modules/ohm-js');
+// const assert = require('assert');
+// const fs = require('fs');
+// const parse = require('../parser');
+// require('../generator');
+// const sinon = require('sinon');
+//
+// const userInput = 'def x = 5;';
+//
+// /* eslint-disable */
+// describe('Generate skrt def ', () => {
+//   it("should return true for 'def x = 5'", () => {
+//     var stub = sinon.stub(),
+//         opts = { call: function (msg) { console.log(msg); } };
+//     stub.gen().returns("World");
+//
+//     // assert(callback.called);
+//   });
+// });
+
 const assert = require('assert');
-const fs = require('fs');
 const parse = require('../parser');
 require('../generator');
-const sinon = require('sinon');
 
-const userInput = 'def x = 5;';
+describe('SKRT Generator', () => {
+  it('def add x2 y2 => { x2 + y2 }', () => {
+    const program = parse('def add x2 y2 => { x2 + y2 }');
+    program.gen();
+    const expected = 'function add_1(x2_2, y2_3) {x2_2 + y2_3}';
+    assert.equal(console.string, expected);
+  });
 
-/* eslint-disable */
-describe('Generate skrt def ', () => {
-  it("should return true for 'def x = 5'", () => {
-    var stub = sinon.stub(),
-        opts = { call: function (msg) { console.log(msg); } };
-    stub.gen().returns("World");
+  it('def x = 5;', () => {
+    const program = parse('def x = 5');
+    program.gen();
+    const expected = 'let x = 5;';
+    assert.equal(console.string, expected);
+  });
 
-    // assert(callback.called);
+  it('def add x2 y2 => { x2 + y2 }', () => {
+    const program = parse('def add x2 y2 => { x2 + y2 }');
+    program.gen();
+    const expected = 'function add_1(x2_2, y2_3) {x2_2 + y2_3}';
+    assert.equal(console.string, expected);
   });
 });
