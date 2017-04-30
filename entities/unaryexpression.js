2e294d2295f94ebd6c37c3efd43e77a85abc7f2d
Ray Toal <rtoal@lmu.edu>
2017-04-29 19:24:37 -0700

    Add optimization method for unary expressions

--- entities/unaryexpression.js
+++ entities/unaryexpression.js
@@ -1,3 +1,6 @@
+const BooleanLiteral = require('./boolean');
+const NumericLiteral = require('./integer');
+
 module.exports = class UnaryExpression {
   constructor(op, operand) {
     this.op = op;
@@ -9,5 +12,14 @@ module.exports = class UnaryExpression {
   analyze(context) {
     this.op.analyze(context);
     this.operand.analyze(context);
+  }
+  optimize() {
+    this.operand = this.operand.optimize();
+    if (this.op === 'not' && this.operand instanceof BooleanLiteral) {
+      return BooleanLiteral(!this.operand.value);
+    } else if (this.op === '-' && this.operand instanceof NumericLiteral) {
+      return new NumericLiteral(-this.operand.value);
+    }
+    return this;
   }
 };
