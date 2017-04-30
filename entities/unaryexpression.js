d9383494a7e29cefc3c6e19891efc26b9a2c32a4
Kevin  Hamilton <khamilt7@lion.lmu.edu>
2017-04-29 21:07:05 -0700

    added the ops for everything but unary

--- entities/unaryexpression.js
+++ entities/unaryexpression.js
@@ -1,3 +1,6 @@
+const BooleanLiteral = require('./boolean');
+const NumericLiteral = require('./integer');
+
 module.exports = class UnaryExpression {
   constructor(op, operand) {
     this.op = op;
@@ -10,10 +13,13 @@ module.exports = class UnaryExpression {
     this.op.analyze(context);
     this.operand.analyze(context);
   }
-
   optimize() {
-    this.op = this.op.optimize();
     this.operand = this.operand.optimize();
+    if (this.op === 'not' && this.operand instanceof BooleanLiteral) {
+      return BooleanLiteral(!this.operand.value);
+    } else if (this.op === '-' && this.operand instanceof NumericLiteral) {
+      return new NumericLiteral(-this.operand.value);
+    }
     return this;
   }
 };
